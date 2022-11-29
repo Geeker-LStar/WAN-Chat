314dcad1036a86f99c9d4eb24f45f954c54da57f
李天星 LStar <103357243+Geeker-LStar@users.noreply.github.com>
2022-11-29 23:05:53 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$line= "xka-xsg";  // production
+$pagetitle="My heart belongs to you.";
+$des = "<h2 style=\"font-family: 'nlt';\">❤️小帅锅和小可爱的聊天室吖~❤️</h2><h2 style=\"font-family: 'nlt';\">Nothing else, just because we love each other!</h2></center><center><h1><a href=\"https://ltx1102.com/xka-xsg/love.php\" style=\"font-family: 'zyy_xmx';\" id=\"reload_trigger\" class=\"reload_trigger\">点我刷新~&rarr;</a><br><a href='//ltx1102.com/xka-xsg/upload.php' style=\"font-family:'nlt';\">上传文件</a></h1></center>";
+$title="欢迎使用！";
+$dbname="";
+$username="";
+$password="";
+
+?>
