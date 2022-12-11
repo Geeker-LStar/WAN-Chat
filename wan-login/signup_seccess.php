67ad2580c87c153b2a5dc8a4e77db5af6c496923
李天星 LStar <103357243+Geeker-LStar@users.noreply.github.com>
2022-12-11 22:49:03 +0800

    Create signup_seccess.php

---
+++ wan-login/signup_seccess.php
@@ -0,0 +1,31 @@
+<!-- 注册成功页面（跳转登录） -->
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>注册成功！</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../wan-static/css/footer.css">
+        <link rel="stylesheet" href="../wan-static/css/login.css">
+
+        <style>
+            h1 {
+                padding-top: 18%;
+            }
+        </style>
+    </head>
+
+    <body>
+        <div class="sign">
+            <div class="main">
+                <h1>注册成功！现在，去登录，并享受 WAN 的世界吧！！</h1>
+                <h2><a href="signin.php">去登录</a></h2>
+            </div>
+            <!--<div class="footer">-->
+            <!--    <p>© 2022 <a href="https://czhiming.cn">曹智铭</a> 和 <a href="https://ltx1102.com">李天星</a> 保留所有权利 | <a href="../SiteMap/sitemap.html">站点地图</a></p>-->
+            <!--    <p><a href="https://beian.miit.gov.cn/#/Integrated/index">京ICP备2021015079号</a> | <a href="https://icp.gov.moe/?keyword=20221320">萌ICP备20221320号</a></p>-->
+            <!--    <p>代码开源地址：<a href="https://github.com/Geeker-LStar/LOVE_czm">LStar's GitHub</a></p>-->
+            <!--</div>-->
+        </div>
+    </body>
+</html>
