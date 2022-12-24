a1030dd117a37735e97b086dc59dfb862fc1b6f2
李天星 LStar <103357243+Geeker-LStar@users.noreply.github.com>
2022-12-24 23:09:56 +0800

    Update join.php

--- wan-gchat/join.php
+++ wan-gchat/join.php
@@ -1,15 +1,15 @@
 <!--加入群聊-->
 
 <!DOCTYPE html>
-<?php 
-    session_start();
-?>
-<?php require "../wan-config.php";?>
-<?php require "../wan-userinfo.php";?>    <!--用户个人信息-->
-
 <html>
     <head>
-        <title>加入群聊</title>
+        <?php 
+            session_start();
+        ?>
+        <?php require_once "../wan-config.php";?>
+        <?php require_once "../wan-userinfo.php";?>    <!--用户个人信息-->
+
+        <title>加入群聊 - WAN</title>
         <meta charset="utf-8">
         <style>
             .must {
@@ -19,11 +19,18 @@
     </head>
     
     <body>
-        <h1>加入群聊</h1>
-        <a href="../wan-myself/myself.php">返回个人中心</a><br><br>
+        <div class="container p-5">
+        <center><h1>加入群聊</h1></center></div>
+        <nav class="navbar navbar-expand-sm bg-light">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" id="reload_trigger" href="../wan-myself/myself.php" style="padding-left: 30px;">&larr;返回个人中心</a>
+                </li>
+            </ul>
+        </nav><br>
         <form method="post" action="join.php">
-            群 id：<input type="text" name="gid"><br><br>
-            <input type="submit" name="搜索" value="搜索">
+            群 id：<input class="form-control" type="text" name="gid"><br><br>
+            <button type="submit" name="搜索" value="搜索">搜索</button>
         </form>
         
         <!--检测是否登录，未登录则要求登录-->
@@ -41,12 +48,12 @@
                 die("连接失败：" . $conn->connect_error);
         ?>
         
-        <!--检索并显示群信息，询问用户是否确认加入-->
+        <!--检索并显示群信息，询问用户是否申请加入-->
         <?php
             $gid = "";
             $iErr = "";
             
-            if (isset($_POST["搜索"]))
+            if (isset($_POST["gid"]))
             {
                 if (empty($_POST["gid"]))
                     $iErr = "填写群 id 才可以搜索呀~";
@@ -70,8 +77,8 @@
                             // 用户已在群聊中
                             if ($members[$i] == $wid)
                             {
-                                echo "<br>您已经加入该群聊啦~";
-                                echo "<br><br><a href='mine.php'>查看我已经加入的群聊</a>";
+                                echo "<br><div class='alert alert-info'>您已经加入该群聊啦~";
+                                echo "<br><br><a href='mine.php'>查看我已经加入的群聊</a></div>";
                                 $already = 1;
                                 break;
                             }
@@ -79,13 +86,13 @@
                         // 用户未在群聊中
                         if (!$already)
                         {
-                            echo "<br>群信息如下：<br><br>";
+                            echo "<div class=<br>群信息如下：<br><br>";
                             echo "<li>群 id：" . $gid . "</li>";
                             echo "<br><li>群名称：" . $gn . "</li><br>";
                             // 隐藏传值（传 $gid）
                             echo "<form method='post' action='join.php'>
                                     <input type='hidden' name='gid' value='$gid'>    
-                                    <input type='submit' name='确认加入' value='确认加入'>
+                                    <input type='submit' name='申请加入' value='申请加入'>
                                 </form>";
                         }
                     } 
@@ -96,24 +103,27 @@
             }
         ?>
         
-        <!--如果用户确认加入-->
+        <!--申请加入（未完成）-->
         <?php
-            if (isset($_POST["确认加入"]))
+            if (isset($_POST["申请加入"]))
             {
                 $gid = $_POST["gid"];    // 获取隐藏传来的值
-                // 在该群聊信息的 “用户” 一项中加入该用户
                 $sql = "SELECT * FROM All_Groups_Info WHERE wan_gid='{$gid}'";
                 $result = $conn->query($sql);
-                $old_gm = $result->fetch_assoc()["g_members"];    // 之前的用户名单
-                $new_g_members = $old_gm . $wid . "//";
-                $sql = "UPDATE All_Groups_Info SET g_members='{$new_g_members}' WHERE wan_gid='{$gid}'";    // 更新
-                $conn->query($sql);
-                // 在该用户的【个人信息——加入的群聊】中加入该群聊
-                $new_g = $mygrp . $gid . "//" ;
-                $sql = "UPDATE Users SET my_groups='{$new_g}' WHERE wan_uid='{$wid}'";
-                $conn->query($sql);
-                header("location: mine.php");
+                $g_mngs = $result->fetch_assoc()["g_managers"];    // 群管理
+                $mngs_lst = explode("//", $g_mngs);    // 群管理列表
+                $time = date("Y-m-d H:i:s");
+                for ($i = 0; $i <= count($mngs_lst)-2; $i++)
+                {
+                    // 在 Group_Verify 中插入该消息的详细信息
+                    $sql = "INSERT INTO Group_Verify (sender, receiver, vrfmsg, state, kind, isread, time, global) VALUES ('{$wid}', '{$mngs_lst[$i]}', '{$gid}', '待确认', 'join_group', '0', '{$time}', '0')";
+                    $conn->query($sql);
+                }
+                echo "<script>
+                        alert('申请成功，等待群管理者确认！');
+                    </script>";
             }
         ?>
+        </div>
     </body>
 </html>
