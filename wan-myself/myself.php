bb374dc7c55cb45c02a944d17a7abb9a825a4dab
李天星 LStar <103357243+Geeker-LStar@users.noreply.github.com>
2022-12-30 23:34:16 +0800

    Update myself.php

--- wan-myself/myself.php
+++ wan-myself/myself.php
@@ -3,12 +3,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-<?php
-    session_start();
-?>
-<?php require_once "../wan-config.php";?>
-<?php require_once "../wan-userinfo.php";?>    <!--用户个人信息-->
-  <?php
+        <?php
+            session_start();
+        ?>
+        <?php require_once "../wan-config.php";?>
+        <?php require_once "../wan-userinfo.php";?>    <!--用户个人信息-->
+        <?php
             date_default_timezone_set("PRC");    // 时区
             // 创建连接
             $conn = new mysqli($servername, $username, $password, $dbname);
@@ -16,11 +16,24 @@
             if ($conn->connect_error)
                 die("连接失败：" . $conn->connect_error);
         ?>
+        
         <title><?php echo $usern;?> 的个人中心 - WAN</title>
         <meta charset="utf-8">
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta http-equiv="Pragma" content="no-cache" />
+        <meta http-equiv="Expires" content="0" />
     </head>
     
     <body>
+        <style>
+            /* 消息省略显示 */
+            .short {
+                width: 100px;
+                overflow: hidden;
+                white-space: nowrap;
+                text-overflow: ellipsis;
+            }
+        </style>
         <!--检测是否登录，未登录则要求登录-->
         <?php
             if (!isset($_SESSION["wuid"]))
@@ -33,14 +46,20 @@
         <h2>个人信息</h2>
         <?php
         
-        echo "<small><p style=\"color: gray\" data-bs-toggle=\"tooltip\" title=\"用户的唯一 ID，WAN 通过这个区分用户。\">wan-uid：{$wid}</p></small>
-            <script>
-            var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle=\"tooltip\"]'))
-            var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-              return new bootstrap.Tooltip(tooltipTriggerEl)
-            })
-            </script>";
-
+            echo "<small><p style=\"color: gray\" data-bs-toggle=\"tooltip\" title=\"用户的唯一 ID，WAN 通过这个区分用户。\">wan-uid：{$wid}</p></small>
+                <script>
+                var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle=\"tooltip\"]'))
+                var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+                  return new bootstrap.Tooltip(tooltipTriggerEl)
+                })
+                </script>";
+            $time = time();    // 当前时间戳（作用：每次都会刷新图片，不会被缓存干扰）
+            echo "个人头像：<br><br>";
+            if (file_exists("headimg/" . $wid . ".png"))
+                echo "<img src='headimg/$wid.png?timestamp=$time' style='height: 100px; width: 100px;'><br><br>";
+            else
+                echo "<img src='headimg/wan-default-head.png' style='height: 100px; width: 100px;'><br><br>";
+            echo "<button class='btn btn-primary' onclick='window.location.href=\"headimg.php\";'>设置和修改头像</button><br><br>";
             echo "手机号：" . $phone;
             echo "<br><br>用户名：" . $usern;
             echo "<br><br>显示名：" . $shown;
@@ -66,66 +85,66 @@
         ?>
         <button class="btn btn-primary" onclick="window.location.href='../wan-gchat/create.php';">新建群聊</button><br><br>
         <button class="btn btn-primary" onclick="window.location.href='../wan-gchat/join.php';">加入群聊</button><br><br>
-        <button class="btn btn-primary" onclick="window.location.href='../wan-gchat/mine.php';">查看已加入的群聊</button><br><br>
+        <button class="btn btn-primary" onclick="window.location.href='../wan-gchat/mine.php';">已加入的群聊</button><br><br>
         <button class="btn btn-primary" onclick="window.location.href='../wan-gchat/groupvrf.php';">群验证消息</button><span> （未读 <?php echo($msg_nums);?> 条，未确认 <?php echo($tobecfm);?> 条）</span><br><br>
         <button class="btn btn-primary" onclick="window.location.href='../wan-users/search.php';">查找用户</button><br><br>
         <h2>最新的消息</h2>
-        <?php /* Emmm... Not an easy task...;)
-        We would get a few groups of this user, and then fetch the latest msg in those groups, put them here and give them a link to directly go to the groups. Ahhhhhhhh...*/
-        
-        $fetchg = "SELECT * FROM Users WHERE wan_uid='{$wid}'";
-        $result2 = $conn->query($fetchg);
-        $all_group = $result2->fetch_assoc();
+        <?php 
+            /* Emmm... Not an easy task...;)
+            We would get a few groups of this user, and then fetch the latest msg in those groups, put them here and give them a link to directly go to the groups. Ahhhhhhhh...*/
+            $sql = "SELECT * FROM Users WHERE wan_uid='{$wid}'";
+            $result2 = $conn->query($sql);
+            $all_group = $result2->fetch_assoc()["my_groups"];
             if (empty($all_group))
                 echo "您暂未加入任何群聊。不妨<a href='//wan.ltx1102.com/wan-gchat/join.php'>加入一个群组</a>！";
             else
             {
-                $g_arr = explode("//",$all_group["my_groups"]);
+                $g_arr = explode("//", $all_group);
                 $g_arr_len = count($g_arr);
-                for($x=0;$x<$g_arr_len-2;$x++)
-                {
-                     $getsepg = "SELECT * FROM All_Groups_Info WHERE wan_gid='{$g_arr[$i]}'";
-                    $result3 = $conn->query($getsepg);
-                    $g_self = $result3->fetch_assoc();
-                    $gname = $g_self["g_name"];
-                    $gid = $g_arr[$x];
-                    $get_into_g = "SELECT * FROM group_$gid ORDER BY msgid DESC LIMIT 1";
-                    $get_into_g_data = $conn->query($get_into_g)->fetch_assoc();
-                    $whosaidit = $get_into_g_data["who"];
-                    $whatsaid = $get_into_g_data["msg"];
-                    echo "<h4>{$gname}</h4><b>{$whosaidit}</b>说：{$whatsaid}<br>";
+                for($i = 0; $i <= $g_arr_len-2; $i++)
+                {  
+                    $sql = "SELECT * FROM All_Groups_Info WHERE wan_gid='{$g_arr[$i]}'";
+                    $result = $conn->query($sql);
+                    $gn = $result->fetch_assoc()["g_name"];
+                    $sql = "SELECT * FROM group_{$g_arr[$i]} ORDER BY msgid desc LIMIT 1";
+                    $result = $conn->query($sql);
+                    $arr = $result->fetch_assoc();
+                    $who = $arr["who"];
+                    $msg = $arr["msg"];
+                    $sql = "SELECT * FROM Users WHERE wan_uid='{$who}'";
+                    $result = $conn->query($sql);
+                    $who_shown = $result->fetch_assoc()["showname"];
+                    echo "<span style='color: skyblue;'>（{$gn}）</span><h4 style='display: inline-block;'>{$gname}</h4><b>{$who_shown}</b><span class='short'>说：{$msg}</span><br>";
                 }
                 echo "<a href='../wan-gchat/mine.php'>去看看</a><br>";
             }
         ?>
-        
-        
         <script>
             var nowtime = new Date;
             var hournow = nowtime.getHours();
             var greeting;
-            if((hournow >= 0 && hournow <= 4)||(hournow >= 20 && hournow <= 24)){
+            if((hournow >= 0 && hournow <= 4)||(hournow >= 20 && hournow <= 24)) {
                 greeting = "晚上";    // 本来想写“夜猫子”。。。
             }
-            else if(hournow >= 5 && hournow <= 7){
+            else if(hournow >= 5 && hournow <= 7) {
                 greeting = "早上";
             }
-            else if(hournow >= 8 && hournow <= 11){
+            else if(hournow >= 8 && hournow <= 11) {
                 greeting = "上午";
             }
-            else if(hournow >= 12 && hournow <= 13){
+            else if(hournow >= 12 && hournow <= 13) {
                 greeting = "中午";
             }
-            else if(hournow >= 14 && hournow <= 19){
+            else if(hournow >= 14 && hournow <= 19) {
                 greeting = "下午";
             }
             else{
                 greeting = "你"; // 虽然我不认为会有这样的情况，但是如果真的出错了，获取不到时间，那就显示“你好”。
             }
             document.getElementById("greeting_auto").innerHTML = greeting;
-            // var gender_db = "<?php echo $gender;?>";
-            // var gender_formatted = ((gender_db=="XX")?"姐姐":"哥哥");
-            // document.getElementById("gender_auto").innerHTML = gender_formatted;
+            var gender_db = "<?php echo $gender;?>";
+            var gender_formatted = ((gender_db=="XX")?"姐姐":"哥哥");
+            document.getElementById("gender_auto").innerHTML = gender_formatted;
         </script>
         </div>
         
