6de03e067479ced846b174c9ace2669cd6147059
李天星 LStar <103357243+Geeker-LStar@users.noreply.github.com>
2023-01-09 22:57:09 +0800

    Create index.php

---
+++ wan-website/index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html><html>
+<!-- Copyright WAN. -->
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="//ltx1102.com/wan/wan-includes/node_modules/bootstrap/dist/css/bootstrap.min.css" />
+        <script src="//ltx1102.com/wan/wan-includes/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+        <meta name="people" content="LTX_CZM">
+        <meta name="generator" content="WAN-Website">
+        <title>WAN - 官方网站</title>
+    </head>
+    <body>
+        
+          <div class="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+      <a href="index.php" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"><img src="./img/wan-logo.png" width="70" height="70">
+      </a>
+
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="#" class="nav-link px-2 link-secondary">主页</a></li>
+        <li><a href="../wan-gchat/mine.php" class="nav-link px-2 link-dark">群聊</a></li>
+        <li><a href="../wan-myself/myself.php" class="nav-link px-2 link-dark">个人中心</a></li>
+        <li><a href="../wan-gchat/join.php" class="nav-link px-2 link-dark">加入群聊</a></li>
+      </ul>
+
+      <div class="col-md-3 text-end">
+        <button type="button" class="btn btn-outline-primary me-2" onclick="window.location.href='../wan-login/signin.php';">登录</button>
+        <button type="button" class="btn btn-primary" onclick="window.location.href='../wan-login/signup.php'">注册</button>
+      </div>
+    </header>
+  </div>
+  
+    <div class="px-4 py-5 my-5 text-center">
+    <img class="d-block mx-auto mb-4" src="./img/wan-logo.png" width="80" height="80">
+    <h1 class="display-5 fw-bold">WAN网络聊天室</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">WAN，一款轻量级的Web聊天室。</p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button type="button" class="btn btn-primary btn-lg px-4 gap-3" onclick="window.location.href='../wan-login/signup.php';">注册</button>
+        <button type="button" class="btn btn-outline-secondary btn-lg px-4" onclick="window.location.href='../wan-login/signin.php';">登录</button>
+      </div>
+    </div>
+  </div>
+    <div class="px-4 pt-5 my-5 text-center border-bottom">
+    <h1 class="display-4 fw-bold">为什么选择WAN？</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">WAN加载速度迅疾，并集成了大量功能，支持个性化，无需安装任何app，在任何设备上显示良好，要求性能配置低……总之，它是你的不二之选。</p>
+    </div>
+    <div class="overflow-hidden" style="max-height: 30vh;">
+      <div class="container px-5">
+        <img src="./img/remote.jpeg" class="img-fluid border rounded-3 shadow-lg mb-4" width="700" height="500" loading="lazy">
+      </div>
+    </div>
+    </div>
+     <div class="px-4 pt-5 my-5 text-center border-bottom">
+         <h1>快得吓人。快得很吓人。</h1>
+          <div class="col-lg-6 mx-auto">
+          <p class="lead mb-4">WAN的净加载速度很快。JavaScript、CSS均使用Minified版本，不仅提速，而且减小吞吐量。我们还应用了多种加载技术，使得加载速度只有微信的20%——即使是在服务器高峰期也不例外。</p>
+    </div>
+  <div class="progress">
+  <div class="progress-bar" role="progressbar" style="width: 20%;" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">WAN 1s</div></div><br>
+  <div class="progress">
+  <div class="progress-bar bg-danger" role="progressbar" style="width: 100%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">微信 5s</div>
+</div>
+<div class="container my-5">
+    <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 shadow-lg">
+      <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+        <h1 class="display-4 fw-bold lh-1">难受想哭？给你动力！</h1>
+        <p class="lead">我们的输入框高度集成了自主研发的KeepFighting API，海量的名言警句、名人名言，搭配我们促进舒缓心情的设计，给你以心态的支持和帮助。</p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold">进一步了解</button>
+        </div>
+      </div>
+      <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+          <img class="rounded-lg-3" src="./img/keepfighting.jpeg" alt="" width="500">
+      </div>
+    </div>
+  </div>
+    <div class="px-4 pt-5 my-5 text-center border-bottom">
+    <h1>一边聊着今天的天，一边回复昨天对方的灵魂拷问……</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">通过WAN心跳技术，WAN的聊天室会进行自动刷新（得益于我们对低配置的技术支持，这并不会弄垮服务器）。确保你的消息总是最新的。再得益于我们的“引用”功能，无论是辩论、商谈还是闲聊，再也不会出现歧义和指代不清的尴尬！</p>
+    </div>
+    <div class="overflow-hidden" style="max-height: 30vh;">
+      <div class="container px-5">
+        <img src="./img/quote.jpeg" class="img-fluid border rounded-3 shadow-lg mb-4" width="700" height="500" loading="lazy">
+      </div>
+    </div>
+    </div>
+    
+    <div class="container my-5">
+    <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 shadow-lg">
+      <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+          <img class="rounded-lg-3" src="./img/myself-fixed.jpeg" alt="" width="500">
+      </div>
+        <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+        <h1 class="display-4 fw-bold lh-1">我就是我，不一样的烟火。</h1>
+        <p class="lead">WAN个人中心有着亲切问候、常用功能和最新消息。它也是你展示个性的平台！</p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+          <button onclick="window.location.href='../wan-myself/myself.php';" type="button" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold">前往个人中心</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="container my-5">
+    <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 shadow-lg">
+      <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+        <h1 class="display-4 fw-bold lh-1">WAN + API = ∞</h1>
+        <p class="lead">WAN提供标准API，通过与WAN接口交互，你可以将几乎你喜爱的任何应用连接到WAN服务。你甚至可以制作一个连接到WAN的第三方客户端，比如图中的私有WAN客户端MHBTY。</p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold">API文档</button>
+        </div>
+      </div>
+      <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+          <img class="rounded-lg-3" src="./img/remote.jpeg" alt="" width="400">
+      </div>
+    </div>
+  </div>
+    <div class="container my-5">
+    <div class="bg-dark text-white row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 shadow-lg">
+      <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+          <img class="rounded-lg-3" src="./img/dark.jpeg" alt="" width="500">
+      </div>
+        <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+        <h1 class="display-4 fw-bold lh-1">暗黑模式不仅仅是黑。</h1>
+        <p class="lead">我们可不想让用户在半夜打开WAN的时候因此失明。因此，WAN采用WPPOOL设计的暗黑模式，自适应系统、记住明暗模式自然是不在话下，而且并不仅仅是将页面反转，通过专业的设计，使得暗黑模式的视觉感受更舒适、更漂亮。原先的彩色将闪耀荧光。</p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+          <button onclick="window.location.href='https://wppool.com';" type="button" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold">在WPPOOL的网站上了解更多</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+    <div class="px-4 py-5 my-5 text-center">
+    
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">心动了吗？快来尝试WAN吧，你会爱上它的。</p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button type="button" class="btn btn-primary btn-lg px-4 gap-3" onclick="window.location.href='../wan-login/signup.php';">注册</button>
+        <button type="button" class="btn btn-outline-secondary btn-lg px-4" onclick="window.location.href='../wan-login/signin.php';">登录</button>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p class="col-md-4 mb-0 text-muted">&copy; 2022-<?php echo date("Y");?> WAN</p>
+
+    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"><img src="./img/wan-logo.png" width="70" height="70">
+    </a>
+
+    <ul class="nav col-md-4 justify-content-end">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">主页</a></li>
+      <li class="nav-item"><a href="../wan-gchat/mine.php" class="nav-link px-2 text-muted">群聊</a></li>
+      <li class="nav-item"><a href="../wan-myself/myself.php" class="nav-link px-2 text-muted">个人中心</a></li>
+      <li class="nav-item"><a href="../wan-login/signin.php" class="nav-link px-2 text-muted">登录</a></li>
+    </ul>
+  </footer>
+</div>
+    </body>
+</html>
