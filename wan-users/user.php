fa45415d6ab3224ade14e1ef626c05d75c211a25
李天星 LStar <103357243+Geeker-LStar@users.noreply.github.com>
2023-01-15 22:40:13 +0800

    Update user.php

--- wan-users/user.php
+++ wan-users/user.php
@@ -1,180 +1,65 @@
+<!--查找用户-->
+
 <!DOCTYPE html>
 <html>
     <head>
-    <?php
-        session_start();
-    ?>
-    <?php require_once "../wan-config.php";?>
-    <?php require_once "../wan-userinfo.php";?>    <!--用户个人信息-->
-    <?php
-        date_default_timezone_set("PRC");    // 时区
-        // 创建连接
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        // 检测连接
-        if ($conn->connect_error)
-            die("连接失败：" . $conn->connect_error);
-        else
-        {
-            $sql = "SELECT * from Users WHERE wan_uid='{$_GET['wid']}'";
-            $result = $conn->query($sql);
-            if ($result->num_rows == 0)
-                echo "<div class='container p-5'><center><h1>该用户不存在！</h1></center></div>";
-            else
-            {
-                $user_arr = $result->fetch_assoc();
-                $uwid = $user_arr["wan_uid"];
-                $uphone = $user_arr["phone"];
-                $uusern = $user_arr["username"];
-                $ushown = $user_arr["showname"];
-                $uemail = $user_arr["email"];
-                $ugender = $user_arr["gender"];
-                $uintrod = $user_arr["self_introd"];
-            }
-        }
-    ?>
-        
-        <title><?php echo $ushown;?> - WAN 发现用户</title>
+        <?php 
+            session_start();
+        ?>
+        <?php require_once "../wan-config.php";?>
+        <?php require_once "../wan-userinfo.php";?>    <!--用户个人信息-->
+
+        <title>查找用户</title>
         <meta charset="utf-8">
     </head>
     
     <body>
-        <!--打开模态框函数-->
-        <script>
-            function open_modal(modal_name) {
-                $(document).ready(function(){ $('#'+modal_name).modal('show'); });
-            }
-            
-        </script>
+        <div class="container p-5">
+        <center><h1>查找用户</h1></center></div>
+        <nav class="navbar navbar-expand-sm bg-light">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" id="reload_trigger" href="../wan-myself/myself.php" style="padding-left: 30px;">&larr;返回个人中心</a>
+                </li>
+            </ul>
+        </nav><br>
+        <!--<div class="form-floating mt-3 mb-3">-->
+        <!--  对方的 wan-uid：<input type="text" class="form-control" id="uwid" placeholder="输入" name="uwid">-->
+        <!--  <label for="uwid">对方的WAN-UID</label>-->
+        <!--</div>-->
+        <center>对方的 wan-uid：<br><br><input type="text" class="col-8 col-sm-7 col-md-6 col-lg-4 col-xl-3 col-xxl-3" id="uwid" name="uwid" class="form-control"><br><br>
+        
+        <!--<label for="uwid">对方的WAN-UID</label>-->
+        <button class="btn btn-primary" onclick="go();" name="搜索" value="搜索">搜索</button><br><br>
+        <div class="alert alert-warning alert-dismissible col-sm-11 col-md-10 col-lg-8 col-xl-7 col-xxl-6">
+            <p style="margin: 0px;">你可能想认识：
+            <a href="https://wan.ltx1102.com/wan-users/user.php?wid=218">曹智铭</a>
+            <span style="padding-left: 10px;"></span>
+            <a href="https://wan.ltx1102.com/wan-users/user.php?wid=1102">李天星</a>
+            <span>（本网站的两位开发者）</span></p></center>
+        </div>
         <!--检测是否登录，未登录则要求登录-->
         <?php
             if (!isset($_SESSION["wuid"]))
                 header("location: ../wan-login/signin.php");
-            if(!isset($_GET["wid"]))
-                header("location: search.php");
         ?>
-    
-        <!-- 填写验证消息模态框 -->
-        <div class="modal fade" id="verification">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title">验证</h4>
-                        <button type="button" class="btn btn-close" data-bs-dismiss="modal"></button>
-                    </div>
-                    <div class="modal-body">
-                        <p>请填写您的验证消息，并点击确认按钮：</p>
-                        <form method="GET" action="user.php">
-                            <input type="hidden" name="wid" value=<?php echo $uwid; ?>>
-                            <input type="text" name="verify_itd" value="我是<?php echo $shown;?>...">
-                            <input type="submit" name="add_as_frd" value="确认">
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">取消</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-        <!-- 申请发送成功模态框 -->
-        <div class="modal fade" id="vrf_ok">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title">成功！</h4>
-                        <button type="button" class="btn btn-close" data-bs-dismiss="modal" onclick="window.location.href = 'user.php?wid=<?php echo $uwid; ?>'"></button>
-                    </div>
-                    <div class="modal-body">
-                        <p>您的好友请求已发送，等待对方确认啦~</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick="window.location.href = 'user.php?wid=<?php echo $uwid; ?>'">关闭</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-        <br>
-        <nav class="navbar navbar-expand-sm bg-light">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                <a class="nav-link" id="reload_trigger" href="search.php" style="padding-left: 30px;">&larr;返回查找用户</a>
-                </li>
-            </ul>
-        </nav><br>
-        
-        <?php if ($uwid) { ?>
-            <div class="container p-5">
-                <h1><?php echo $ushown;?><span id="gender_auto"></span></h1>
-                <h2>个人信息</h2>
-                <?php
-                
-                    echo "<small><p style=\"color: gray\" data-bs-toggle=\"tooltip\" title=\"用户的唯一ID，WAN通过这个区分用户。\">wan-uid：{$uwid}</p></small>
-                    <script>
-                    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle=\"tooltip\"]'))
-                    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-                      return new bootstrap.Tooltip(tooltipTriggerEl)
-                    })
-                    </script>";
-        
-                    
-                    // echo "手机号：" . $uphone;
-                    // echo "<br><br>用户名：" . $uusern;
-                    $time = time();    // 当前时间戳（作用：每次都会刷新图片，不会被缓存干扰）
-                    echo "个人头像：<br><br>";
-                    if (file_exists("../wan-myself/headimg/" . $uwid . ".png"))
-                        echo "<img src='../wan-myself/headimg/$uwid.png?timestamp=$time' style='height: 100px; width: 100px;'><br><br>";
-                    else
-                        echo "<img src='../wan-myself/headimg/wan-default-head.png' style='height: 100px; width: 100px;'><br><br>";
-                    echo "显示名：" . $ushown;
-                    if (empty($uemail))
-                        echo "<br><br>邮箱：未填写";
-                    else 
-                        echo "<br><br>邮箱：" . $uemail;
-                    if (empty($uintrod))
-                        echo "<br><br>个人简介：未填写";
-                    else 
-                        echo "<br><br>个人简介：" . $uintrod;
-                ?>
-                <br><br>
-                
-                <?php
-                    // 检索该人是否为该用户的好友，若不是，则显示添加好友，否则显示进入聊天
-                    $all_frd = explode("//", $myfrd);
-                    if (!in_array($uwid, $all_frd)) {
-                ?>
-                    <button type="button" class="btn btn-warning" onclick="open_modal('verification')">加为好友</button>
-                <?php
-                    }
-                    else 
-                        echo("开发中！");
-                ?>
-                <script>
-                    var gender_db = "<?php echo $ugender;?>";
-                    var gender_formatted = ((gender_db=="XX")?"姐姐":"哥哥");
-                    document.getElementById("gender_auto").innerHTML = gender_formatted;
-                </script>
-            </div>
-        <?php } ?>
-        
-        <!--申请加为好友-->
+        <!--连接数据库-->
         <?php
-            if (isset($_GET["add_as_frd"]))
-            {
-                // 在消息表中插入待确认的消息
-                $time = date("Y-m-d H:i:s");
-                $msg = "“<a href=\"../wan-users/user.php?wid=$wid\">" . $shown . "</a>” 申请添加你为好友。";
-                $vrf = $_GET["verify_itd"];    // 身份信息
-                $adding = "验证信息：" . $vrf;
-                $sql = "INSERT INTO Private_Verify (sender, receiver, vrfmsg, detail, state, kind, isread, time) VALUES ('{$wid}', '{$uwid}', '{$msg}', '{$adding}', '待确认', 'add_friend', '0', '{$time}')";
-                if ($conn->query($sql))
-                    echo "<script>open_modal('vrf_ok')</script>";
-                else
-                    header("location: ../wan-static/html/failed.html");
-            }
+            // 创建连接
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // 检测连接
+            if ($conn->connect_error)
+                die("连接失败：" . $conn->connect_error);
         ?>
+        <script>
+            function go() {
+                var whoisthat = document.getElementById("uwid").value;
+                var link = "https://wan.ltx1102.com/wan-users/user.php?wid=" + whoisthat;
+                window.location.href = link;
+            }
+        </script>
         
+        </div>
         <?php require "../wan-footer.php";?>
-    
     </body>
 </html>
